23db03b1aeee5de62c3733305b61de5077e42ee6
jassler <erifetim@gmail.com>
2020-10-26 08:26:51 +0100

    appendTuple added

--- src/de/hskempten/tabulang/datatypes/Tuple.java
+++ src/de/hskempten/tabulang/datatypes/Tuple.java
@@ -12,7 +12,7 @@ public class Tuple {
 
     public Tuple(List<Object> objects) {
         this.objects = objects;
-        this.names = IntStream.range(0, objects.size()).boxed().map(i -> String.valueOf(i)).collect(Collectors.toList());
+        this.names = IntStream.range(0, objects.size()).boxed().map(String::valueOf).collect(Collectors.toList());
     }
 
     public Tuple(List<Object> objects, List<String> names) {
@@ -42,6 +42,15 @@ public class Tuple {
         isHorizontal = horizontal;
     }
 
+    /**
+     * Get tuple element by name or index.
+     * Note that if names are also indexes, the named index will be prioritized over the number index.
+     *
+     * @param name Name or index number of element
+     * @return Element object
+     * @throws NumberFormatException          if name not present and not convertible into a number
+     * @throws ArrayIndexOutOfBoundsException if name not present and converted number is out of range
+     */
     public Object get(String name) {
         int index = names.indexOf(name);
         if (index >= 0)
@@ -58,5 +67,16 @@ public class Tuple {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    /**
+     * Append tuple elements including names to this tuple.
+     * Orientation stays the same.
+     *
+     * @param t Tuple with object elements and names to append
+     */
+    public void append(Tuple t) {
+        objects.addAll(t.getObjects());
+        names.addAll(t.getNames());
     }
 }
