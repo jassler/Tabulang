a87cb018c830070a7588ca182899cba9ea487e86
Simon Staiger <Simon-Staiger@pester.com>
2021-01-06 18:05:08 +0100

    Bugfix

--- src/main/java/de/hskempten/tabulang/standardBibliothek/OpenDbConnection.java
+++ src/main/java/de/hskempten/tabulang/standardBibliothek/OpenDbConnection.java
@@ -1,6 +1,7 @@
 package de.hskempten.tabulang.standardBibliothek;
 
 import de.hskempten.tabulang.datatypes.InternalNumber;
+import de.hskempten.tabulang.datatypes.InternalString;
 import de.hskempten.tabulang.libreOffice.OdsExportService;
 import de.hskempten.tabulang.mySql.DatabaseConnection;
 import de.hskempten.tabulang.mySql.Models.MSqlConnectionParameters;
@@ -9,11 +10,11 @@ public class OpenDbConnection implements InternalFunction {
     @Override
     public Object compute(Object... args) {
         if(Helper.LengthReviewer(5, args)){
-            var ip = (String)args[0];
+            var ip = ((InternalString)args[0]).getString();
             var port = (int) ((InternalNumber)args[1]).getFloatValue();
-            var dbName = (String)args[2];
-            var userName = (String)args[3];
-            var password = (String)args[4];
+            var dbName = ((InternalString)args[2]).getString();
+            var userName = ((InternalString)args[3]).getString();
+            var password = ((InternalString)args[4]).getString();
             var parameters = new MSqlConnectionParameters(ip, port, dbName, userName, password);
             DatabaseConnection.OpenConnection(parameters);
             return true;
