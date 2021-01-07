a32fbff6799f4ed25364e23f8d1bf5965e3f78e1
Benjamin Louisot <louisot@gmx.de>
2021-01-07 22:02:50 +0100

    Fix merge collision

--- src/main/java/de/hskempten/tabulang/standardLibrary/importexport/CloseDbConnection.java
+++ src/main/java/de/hskempten/tabulang/standardLibrary/importexport/CloseDbConnection.java
@@ -1,4 +1,4 @@
-package de.hskempten.tabulang.standardBibliothek;
+package de.hskempten.tabulang.standardLibrary.importexport;
 
 import de.hskempten.tabulang.mySql.DatabaseConnection;
 import de.hskempten.tabulang.standardLibrary.InternalFunction;
@@ -6,8 +6,11 @@ import de.hskempten.tabulang.mySql.Exceptions.MySqlConnectionException;
 
 public class CloseDbConnection implements InternalFunction {
     @Override
-    public Object compute(Object... args) throws MySqlConnectionException {
+    public Object compute(Object... args) {
+
+        // may throw MySqlConnectionException
         DatabaseConnection.CloseConnection();
+
         return null;
     }
 }
