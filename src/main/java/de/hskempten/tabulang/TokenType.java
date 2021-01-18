50d84f310171593f1054bcc443bffd49fb6b69d7
jassler <erifetim@gmail.com>
2021-01-18 01:07:32 +0100

    Removed redundand braces

--- src/main/java/de/hskempten/tabulang/TokenType.java
+++ src/main/java/de/hskempten/tabulang/TokenType.java
@@ -22,22 +22,22 @@ public class TokenType {
     public static final TokenExpression[] TOKEN_EXPRESSIONS;
 
     static {
-        KEYWORD = new TokenExpression("keyword", "(after)|(as)|(average)|(background)|(before)|(count)|" +
-                "(direction)|(distinct)|(else)|(exists)|(false)|(filter)|(forAll)|(fontStyle)|(font)|(foreground)|(for)|(from)|" +
-                "(function)|(group)|(hiding)|(holds)|(horizontal)|(if)|(intersect)|(in)|(mark)|(not)|(null)|" +
-                "(return)|(set)|(size)|(suchThat)|(true)|(unite)|(using)|(var)|(vertical)");
+        KEYWORD = new TokenExpression("keyword", "(after|as|average|background|before|count|" +
+                "direction|distinct|else|exists|false|filter|forAll|fontStyle|font|foreground|for|from|" +
+                "function|group|hiding|holds|horizontal|if|intersect|in|mark|not|null|" +
+                "return|set|size|suchThat|true|unite|using|var|vertical)");
         VARIABLE = new TokenExpression("variable", "[a-zA-Z]\\w*");
         NUMBER = new TokenExpression("number", "-?[0-9]+(\\.[0-9]+)?");
         SEMICOLON = new TokenExpression(";", ";");
         ASSIGN = new TokenExpression(":=", ":=");
-        BINARY_OPERATOR = new TokenExpression("binaryOperator", "\\+|-|\\*|/|(div)|(mod)|\\^");
+        BINARY_OPERATOR = new TokenExpression("binaryOperator", "\\+|-|\\*|/|(div|mod)|\\^");
         BRACKET = new TokenExpression("bracket", "\\{|\\}|\\(|\\)|\\[|\\]");
         COMMA = new TokenExpression("comma", ",");
         INTERVAL = new TokenExpression("interval", "\\.\\.\\.");
         FUNDEF = new TokenExpression("fundef", "->");
         QUOTEDSTRING = new TokenExpression("quotedString", "'[^']*'");
         BINRELSYM = new TokenExpression("binRelSym", "<=|>=|!=|=|<|>");
-        BINBOOL = new TokenExpression("binBool", "(and)|(or)|(xor)|(iff)|(impl)");
+        BINBOOL = new TokenExpression("binBool", "(and|or|xor|iff|impl)");
         DOT = new TokenExpression(".", "\\.");
 
 
