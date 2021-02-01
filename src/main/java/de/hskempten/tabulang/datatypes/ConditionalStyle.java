c5f2e02758d322cf99695e58e66c8c7147093823
jassler <erifetim@gmail.com>
2021-02-01 14:59:59 +0100

    Added docs to ConditionalStyle

--- src/main/java/de/hskempten/tabulang/datatypes/ConditionalStyle.java
+++ src/main/java/de/hskempten/tabulang/datatypes/ConditionalStyle.java
@@ -2,10 +2,15 @@ package de.hskempten.tabulang.datatypes;
 
 import java.util.function.Predicate;
 
+/**
+ * Apply style to object that satisfies a certain condition.
+ * TODO: Not yet supported
+ *
+ * @param <E> Data object type that is passed to the predicate
+ */
 public class ConditionalStyle<E> extends Style {
 
     private final Predicate<E> p;
-
     public ConditionalStyle(Predicate<E> p) {
         this.p = p;
     }
